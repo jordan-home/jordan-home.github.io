04d7cb5b795db952670c9fe0fa79ee44f2b45366
huanggang <huanggang@kezaihui.com>
2021-06-04 17:35:09 +0800

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,20 @@ set -e
 # 生成静态文件
 npm run build
 
+commitTime=`date +'%Y-%m-%d %H:%M:%S'`
+
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
 cd blog
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "build add delploy at $commitTime"
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
 git push -f git@github.com:jordan-home/jordan-home.github.io.git master
+
+cd ../
+
+rm -rf blog
 
 cd -
